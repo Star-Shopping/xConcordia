5f3496a28a4c3422ecff0695c29b29f4ce932bc8
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2022-12-12 00:05:39 -0500

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,9 +1,9 @@
 describe('sample render test', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
+    beforeEach(() => {
+        cy.visit('/');
+    });
 
-  it('displays the title text', () => {
-    cy.get('h1').contains('Next.js Blog Theme');
-  });
+    it('displays the title text', () => {
+        cy.get('h1').contains('xConcordia');
+    });
 });

57bc0d51947189867632269c700cf7b3977b6476
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2022-12-12 00:49:59 -0500

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,14 +1,3 @@
-const headers = new Headers();
-headers.set('520', 'Bearer <276377ba5206683e51d50fb86c378dc5>');
-
-const response = await fetch('https://opendata.concordia.ca/API/v1/library/occupancy/', {
-    method: 'GET',
-    headers: headers,
-});
-
-const json = await response.json();
-console.log(json);
-
 export const getGlobalData = () => {
     const name = process.env.BLOG_NAME ?
         decodeURI(process.env.BLOG_NAME) :

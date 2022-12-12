c19542e67eb9fad8935248f0a3e7455f988000ff
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2022-12-12 00:25:48 -0500

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
         'xConcordia';
     const blogTitle = process.env.BLOG_TITLE ?
         decodeURI(process.env.BLOG_TITLE) :
-        'xConcordia';
+        'yerrrr';
     const footerText = process.env.BLOG_FOOTER_TEXT ?
         decodeURI(process.env.BLOG_FOOTER_TEXT) :
         'yeeeeeeeeee buddyyyyy.';

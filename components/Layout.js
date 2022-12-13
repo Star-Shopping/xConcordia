5f425215115474e01e1570745ef6e4f60a3a0a95
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2022-12-13 00:38:43 -0500

    test

--- components/Layout.js
+++ components/Layout.js
@@ -19,25 +19,6 @@ export function GradientBackground({ variant, className }) {
 }
 
 export default function Layout({ children }) {
-    
-  // const [people, setPeople] = useState([]);
-  // function getPeople() {
-  //   return fetch('API_URL', {
-  //     headers: {
-  //       'Authorization': 'Basic ' + btoa(`${API_USERNAME}:${API_PASSWORD}`)
-  //     }
-  //   } )
-  //   .then(response => console.log(response.json()))
-
-  //   .catch(error => this.handleError(error))
-  // }
-  // useEffect(() => {
-  //   // Call the getPeople function to fetch the list of people
-  //   getPeople().then(people => {
-  //     // Set the people state with the list of people returned by the API
-  //     setPeople(people);
-  //   });
-  // }, []);
 
   const setAppTheme = () => {
     const darkMode = localStorage.getItem('theme') === 'dark';

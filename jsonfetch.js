59bc3c64c52fe5ab86cdcf7e0809a8017cc7bd99
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2022-12-12 22:22:45 -0500

    Update jsonfetch.js

--- jsonfetch.js
+++ jsonfetch.js
@@ -1,19 +1,23 @@
-  const [people, setPeople] = useState([]);
+const API_URL = 'https://opendata.concordia.ca/API/v1/library/occupancy/';
+const API_USERNAME = '520';
+const API_PASSWORD = '276377ba5206683e51d50fb86c378dc5';
 
-  function getPeople() {
-      return fetch('API_URL', {
-              headers: {
-                  'Authorization': 'Basic ' + btoa(`${API_USERNAME}:${API_PASSWORD}`)
-              }
-          })
-          .then(response => console.log(response.json()))
+const [people, setPeople] = useState([]);
 
-      .catch(error => this.handleError(error))
-  }
-  useEffect(() => {
-      // Call the getPeople function to fetch the list of people
-      getPeople().then(people => {
-          // Set the people state with the list of people returned by the API
-          setPeople(people);
-      });
-  }, []);
+function getPeople() {
+    return fetch('API_URL', {
+            headers: {
+                'Authorization': 'Basic ' + btoa(`${API_USERNAME}:${API_PASSWORD}`)
+            }
+        })
+        .then(response => console.log(response.json()))
+
+    .catch(error => this.handleError(error))
+}
+useEffect(() => {
+    // Call the getPeople function to fetch the list of people
+    getPeople().then(people => {
+        // Set the people state with the list of people returned by the API
+        setPeople(people);
+    });
+}, []);

c6a977bb5836b49893e5d71afde6a6947da32d37
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2022-12-12 23:20:30 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Index({ posts, globalData }) {
 const username = '520';
 const password = '276377ba5206683e51d50fb86c378dc5';
 
-axios.get('https://opendata.concordia.ca/API/v1/library/occupancy/', {
+axios.get('https://cors-anywhere.herokuapp.com/https://opendata.concordia.ca/API/v1/library/occupancy/', {
     auth: {
       username,
       password
